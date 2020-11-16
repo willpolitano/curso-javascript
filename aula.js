31ced239a9a2ecf11cc11daaee85a16ffbe2c381
willian politano <sistema2@yescert.com.br>
2020-11-16 12:50:06 -0300

    aula js

---
+++ aula.js
@@ -0,0 +1,73 @@
+// Variaveis 
+//     * Não pode iniciar com numero
+//     * Não pode conter espaços
+//     * Usar camelCase
+//     * É case sensitive
+//     ex : let idade,nomeCompleto,cargo;
+
+
+// Constantes 
+//     * Não pode alterar o valor definido
+//     ex : const valorConstante = 20;
+
+
+// Tipos de variaveis
+//     * String
+//     * Number
+//     * Boolean
+//     * Array
+//     * Object
+//     * Function
+//     ex : 
+//         let nome = 'Will'; String literal
+//         let idade = 25; Number literal
+//         let aprovado = true; Boolean
+//         let sobrenome; Undefined
+//         let cor = null; Redifinir um valor
+
+
+// Tipagem dinamica
+//     * Js possui tipagem dinamica, ou seja, 
+//     não é necessário declarar o tipo de variavel.
+//     * É possivel acessar o tipo de variaver 
+//     por meio do typeof
+//     ex : 
+//     let teste = 'will';
+//     teste = 123;
+//     teste = [];
+//     typeof teste = array
+
+
+// Objetos
+//     ex : 
+//     let pessoa = {
+//         nome : 'Will',
+//         idade : 21
+//     };
+//     console.log(pessoa.nome);
+
+
+// Arrays
+//     ex : 
+//     let numeros = [26,true,'jhonantan',17];
+//     console.log(numeros.lenght)
+
+
+// Function
+//     Verbo + substantivo
+//     ex : 
+//     function resetaCor(){};
+
+
+// Operadores 
+//     * Aritiméticos(+ - * / **)
+//     * Atribuição
+//     * Comparação
+//     * Logicos
+//     * Bitwise
+
+
+
+
+
+
