a1413688913f6c35613f82eaf0b200939abceee3
willpolitano <willianpolitano@gmail.com>
2020-11-17 17:09:32 -0300

    tipos de looping em js

---
+++ exemplo3/index.js
@@ -0,0 +1,79 @@
+               //Loopings em arrays
+
+// FOR 
+
+//     const minhaMesa = [
+//         'notebook',
+//         'garrafinha',
+//         'alcool gel',
+//         'fone',
+//         'suportes'
+//     ]
+
+//     for (let i = 0; i < minhaMesa.length; i++) {
+//         console.log(`${i}. ${minhaMesa[i]}`)
+//     }
+
+
+
+
+// FOR IN  
+
+//     const tecnicos = [{
+//             nome : 'Aline',
+//             polo : 'Anatel'
+//         },
+//         {
+//             nome : 'Thauany',
+//             polo : 'Anatel'
+//         },
+//         {
+//             nome : 'Camila',
+//             polo : 'Inmetro'
+//         },
+//         {
+//             nome : 'Daniele',
+//             polo : 'Inmetro'
+//         }
+//     ]
+
+//     for (let tecnico in tecnicos) {
+//         console.log(tecnicos[tecnico])
+//     }
+
+
+
+// FOREACH
+
+//     const pessoas = [
+//         'Will',
+//         'Fê',
+//         'Dani',
+//         'Milena',
+//         'Eduardo'
+//     ]
+
+//     pessoas.forEach( (value, index) => {
+//         console.log(`${index}. ${value}`)
+//     })
+
+
+
+// MAP
+
+//     const temperaturaCelsius = [0,22,31,45,60,3]
+//     const toFahrnheit = value => ((value * 9) / 5) + 32
+//     const temperaturaFahreneheit = temperaturaCelsius.map(toFahrnheit)
+//     console.log(temperaturaCelsius)
+//     console.log(temperaturaFahreneheit)
+
+
+
+// FILTER 
+
+//     const numeros = [1,2,3,4,5,6,7,8,9,10]
+//     const par = n => n % 2 === 0
+//     const numeorsPar = numeros.filter(par)
+
+//     console.log(`Array com numeros pares ${numeorsPar}`)
+
